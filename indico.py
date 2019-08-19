5ff00794e71d0d17d81bd672fb9adb05b8432718
Philip Chang <sgnoohc@gmail.com>
2019-08-19 11:00:43 -0700

    update

--- indico.py
+++ indico.py
@@ -31,13 +31,19 @@ def build_indico_request(path, params, api_key=None, secret_key=None, only_publi
     return '%s?%s' % (path, urlencode(items))
 
 #__________________________________________________________________________________________________________________________________
-def get_events_from_json(categID):
+def get_events_from_json(categID, categID_filters=[]):
     # Retrieve list of eventIDs (6 digit numbers) from "<categID>.json"
     the_json_file_for_the_categID = open('{0}.json'.format(categID))
     loaded_json_file_for_the_categID = json.load(the_json_file_for_the_categID)
     list_of_events_id = []
     for item in loaded_json_file_for_the_categID['results']:
-        # print item['id'], item['title'], item['startDate']['date']
+        # Filtering specific events based on its parent category ID
+        # e.g. 26 <-- Higgs meetings, 5783 <-- SMP-VV, etc.
+        pass_filter = False
+        if item['categoryId'] in categID_filters:
+            pass_filter = True
+        if not pass_filter:
+            continue
         list_of_events_id.append(item['id'])
     return list_of_events_id
 
@@ -55,7 +61,18 @@ def get_event_ics_str(eventID, detail='events'):
 
 if __name__ == '__main__':
 
-    list_of_events_id = get_events_from_json(26)
+    categIDs_of_interest = [
+            7803, # Tracking POG
+            1576, # Trigger Studies Group
+            1358, # Tracking DPG/POG
+            5783, # SMP-VV
+            26, # Higgs meetings
+            3886, # SMP General
+            5784, # VVV Working meeting
+            677, # CMS General Meeting
+            ]
+
+    list_of_events_id = get_events_from_json(6803, categID_filters=categIDs_of_interest)
 
     for event_id in list_of_events_id:
         print get_event_ics_str(event_id, detail='contributions')
